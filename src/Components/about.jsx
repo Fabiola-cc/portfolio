226d7880802c9521042d3a7106a2be2c9c45b50a
Fabiola-cc <fcontrerascol@gmail.com>
2024-05-26 17:48:53 -0600

    feat: edit about me

--- src/Components/about.jsx
+++ src/Components/about.jsx
@@ -1,9 +1,21 @@
 import './about.css';
 
 const About = () => {
+
   return (
     <div className='about'>
-      <h5 className='title'>Sobre mí</h5>
+      <div className='content'>
+        <div className='image-container'>
+          <img className='picture' src='/src/assets/me.jpg' alt='Me, myself and I' />
+        </div>
+        <div className='text'>
+          <h3>Esta soy yo:</h3>
+          <p>Fabiola Alejandra Contreras Colindres, pero la mayoría me dice Fabi. Actualmente estoy en tercer año de Ingeniería en Ciencia de la Computación y Tecnologías de la Información en la Universidad del Valle de Guatemala .</p>
+          <p>Desde que tengo memoria he estado envuelta en el mundo de la tecnología, mientras yo crecía también lo hacía el smartphone, pero aún mejor siempre pendiente del trabajo de mi papá con tecnología.</p>
+          <p>Por si no lo has notado, me encanta el cielo y voltear a verlo de vez en cuando para despejar mi mente.</p>
+          <a href='https://drive.google.com/file/d/1zdm5_QNzmrkofyZe_bcol2WIoljRoBgn/view?usp=sharing' className='cv-button' target='_blank' rel='noopener noreferrer'>Mi CV</a>
+        </div>
+      </div>
     </div>
   );
 };
