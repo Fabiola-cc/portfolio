168e2216f37ebfecae7611820b843def7c43dc0d
Fabiola-cc <fcontrerascol@gmail.com>
2024-05-30 17:58:27 -0600

    fix: responsive view changes

--- src/Components/Details/clouds.jsx
+++ src/Components/Details/clouds.jsx
@@ -3,9 +3,9 @@ import './clouds.css'
 const Cloud = () => {  
     return (
       <div>
-        <div className="cloud" style={{ top: '15%', left: '10%' }}></div>
-        <div className="cloud" style={{ top: '35%', left: '50%' }}></div>
-        <div className="cloud" style={{ top: '70%', left: '80%' }}></div>
+        <div className="cloud cloud-1" />
+        <div className="cloud cloud-2" />
+        <div className="cloud cloud-3" />
       </div>
     );
   };
--- src/Components/Details/moon.jsx
+++ src/Components/Details/moon.jsx
@@ -1,8 +1,16 @@
 import './moon.css';
 
 const Moon = () => {
+  const backgroundElement = document.querySelector('.background');
+    
+    // Obtener la altura del elemento .background
+    const backgroundHeight = backgroundElement.clientHeight;
+
+    // Calcular la posición vertical de las estrellas (por ejemplo, el 70% del penúltimo color)
+    const position = backgroundHeight * 0.8;
+
   return (
-      <div className="moon"></div>
+      <div className="moon" style={{ top: position}} ></div>
   );
 };
 
